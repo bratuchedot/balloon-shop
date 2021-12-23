c0ca1c06f385e1f0f77b8d0adc0e43175a297dbf
Emilijan Koteski <emilijan.koteski@students.finki.ukim.mk>
2021-12-23 15:28:58 +0100

    Init commit

---
+++ src/main/java/mk/ukim/finki/balloon/shop/BalloonShopApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.balloon.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalloonShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalloonShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/balloon/shop/BalloonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.balloon.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalloonShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
